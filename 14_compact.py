e98f76942e42c60e39898dd93d7abf7f6ed7dc3c
Nathan Rydel <nathanrydel@gmail.com>
2023-11-27 18:16:10 -0700

    14 Passing

--- 14_compact.py
+++ 14_compact.py
@@ -4,3 +4,14 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+
+    # Naive approach
+    # truthy_lst = []
+
+    # for element in lst:
+    #     if element:
+    #         truthy_lst.append(element)
+
+    # return truthy_lst
+
+    return [element for element in lst if element]
