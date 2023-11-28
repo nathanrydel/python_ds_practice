8fd5d814740be395e141cee8ef4d340f93729511
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-27 16:28:25 -0800

    09 passed

--- 09_is_palindrome.py
+++ 09_is_palindrome.py
@@ -21,3 +21,19 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
+
+    phrase = phrase.lower().replace(' ', '')
+
+    i = 0
+    j = len(phrase) - 1
+
+    while i < j:
+
+        if phrase[i] != phrase[j]:
+            return False
+
+        i += 1
+        j -= 1
+
+    return True
+
