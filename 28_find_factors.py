4dac16e5c1e3ef88484c4101e350cd3637ec4caa
Nathan Rydel <nathanrydel@gmail.com>
2023-11-28 13:30:55 -0700

    28 passing on naive

--- 28_find_factors.py
+++ 28_find_factors.py
@@ -13,3 +13,13 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    factors = []
+    n = 1
+
+    while n <= num:
+        if num % n == 0:
+            factors.append(n)
+        n += 1
+
+    return factors
